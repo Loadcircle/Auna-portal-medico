5d80d17a1d5d63e0c8e38531d71652f793e39ca6
Jesus Milano <jesusmilano96@gmail.com>
2018-12-16 20:51:20 -0600

    finished

--- js/main_app.js
+++ js/main_app.js
@@ -1,18 +1,21 @@
 (function(){
     let toggle_menu = document.querySelectorAll('.toggle_menu');
 
-    toggle_menu.forEach(function(k){
-        k.addEventListener('click', function(){
-            this.classList.toggle('active');
+    if(toggle_menu){
+        toggle_menu.forEach(function(k){
+            k.addEventListener('click', function(){
+                this.classList.toggle('active');
+            })
         })
-    })
+    }
 
+    const menu_portal_btn = document.getElementById('deploy_menu_btn');
+    const menu_portal = document.getElementById('deploy_menu');
 
-
-
-
+    if(menu_portal && menu_portal_btn){
+        menu_portal_btn.addEventListener('click', function(){
+            menu_portal.classList.toggle('active');
+        });
+    }
     
 })();
-
-
-
