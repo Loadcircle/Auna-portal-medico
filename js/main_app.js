6a53dc04e95f4ee408f590df506d5cca53bc48f0
Jesus Milano <jesusmilano96@gmail.com>
2018-12-20 11:27:25 -0600

    segunda version

--- js/main_app.js
+++ js/main_app.js
@@ -1,12 +1,16 @@
 (function(){
     let toggle_menu = document.querySelectorAll('.toggle_menu');
-
+    let only_not = document.querySelector('.bell.toggle_menu');
     if(toggle_menu){
         toggle_menu.forEach(function(k){
             k.addEventListener('click', function(){
                 this.classList.toggle('active');
             })
         })
+
+        only_not.addEventListener('click', function(){
+            this.classList.toggle('active_d');
+        });
     }
 
     const menu_portal_btn = document.getElementById('deploy_menu_btn');
