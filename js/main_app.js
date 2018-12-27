c6676f2b6d8ce79314e413a05a9582755454df07
Jesus Milano <jesusmilano96@gmail.com>
2018-12-27 13:01:50 -0600

    faltan graficas

--- js/main_app.js
+++ js/main_app.js
@@ -1,18 +1,26 @@
 (function(){
     let toggle_menu = document.querySelectorAll('.toggle_menu');
     let only_not = document.querySelector('.bell.toggle_menu');
+    let only_not_mob = document.querySelector('nav.only_mobile .bell.toggle_menu');
     if(toggle_menu){
         toggle_menu.forEach(function(k){
             k.addEventListener('click', function(){
                 this.classList.toggle('active');
             })
         })
+        
+        if(only_not){
+            only_not.addEventListener('click', function(){
+                this.classList.toggle('active_d');
+            });
+        }
+    }
 
-        only_not.addEventListener('click', function(){
+    if(only_not_mob){
+        only_not_mob.addEventListener('click', function(){
             this.classList.toggle('active_d');
         });
     }
-
     const menu_portal_btn = document.getElementById('deploy_menu_btn');
     const menu_portal = document.getElementById('deploy_menu');
 
